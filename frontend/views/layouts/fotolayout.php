4c601aa7729052c8a0714fad085c92d12fd1c238
Stepan <kurakste@gmail.com>
2018-12-27 09:17:00 +0300

    Add yandex metricas counter

--- frontend/views/layouts/fotolayout.php
+++ frontend/views/layouts/fotolayout.php
@@ -79,6 +79,22 @@ AppAsset::register($this);
       </div> <!--End row -->
 <!-- End Footer -->
     </div>  <!--End conteiner -->
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+           ym(51729818, "init", {
+                id:51729818,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+           });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/51729818" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
 <?php $this->endBody() ?>
   </body>
 </html>
